009190daec629294b07d6244da2fcdde95efaeb1
shaunfinn <shaunfinneran91@gmail.com>
2020-12-09 09:47:28 +0000

    old stuff deleted

--- webcam_take_photo/take_photo.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def grab_frame():
-    camera = cv2.VideoCapture(0)
-    return_value, image = camera.read()
-    cv2.imwrite('take_photo'+'.png', image)
-    print("image_update")
-    camera.release()
